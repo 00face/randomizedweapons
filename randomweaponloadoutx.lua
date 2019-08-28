b28e11de7ebf04c38e48ad29f5213ecd0adbb174
Armando <adcornaglia@gmail.com>
2019-08-28 18:15:06 -0500

    Update randomweaponloadoutx.lua

--- randomweaponloadoutx.lua
+++ randomweaponloadoutx.lua
@@ -1,39 +1,40 @@
+-- Put your Lua here
 -- 00face
 if SERVER then
 AddCSLuaFile("randomweaponloadout.lua")
 end local weapons = {
-    primary= {
+    primary = {
 {["name"] = "weapon_smg"},
 {["name"] = "weapon_smgx"},
     },
-    
+
     secondary = {
 {["name"] = "weapon_crowbar"},
-{["name"] = "weapon_crowbar"},        
+{["name"] = "weapon_crowbar"},
     },
-    
+
     grenade = {
-        {["name"]="weapon_tttbasegrenade"},
-        {["name"]="weapon_zm_molotov"},
-        {["name"]="weapon_ttt_smokegrenade"}
+        {["name"] = "weapon_tttbasegrenade"},
+        {["name"] = "weapon_zm_molotov"},
+        {["name"] = "weapon_ttt_smokegrenade"}
     },
 }
 
 hook.Add("PlayerLoadout", "GiveWeapon", function(ply)
-  
+hook.Add("PlayerSpawn", "Spawnhook", function(ply)
+
     local primaryweapon = table.Random( weapons.primary )
     local secondaryweapon = table.Random( weapons.secondary )
     local grenade = table.Random( weapons.grenade )
-      
+
     ply:Give(primaryweapon.name)
-      
+
     ply:Give(secondaryweapon.name)
-    
-    noDrop = {"weapon_fists"}    
-    
+
+    noDrop = {"weapon_fists"}
+
     ply:Give(grenade.name)
-    
-    hook.Add("PlayerSpawn", "Spawnhook", function(ply)
-end) 
+
+end)
 
 end)
